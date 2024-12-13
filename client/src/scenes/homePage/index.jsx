8fb5e6a4dedd2a52539fa099220bd9b713441d87
Harsh Mittal <harshmit@amazon.com>
2024-12-13 18:07:14 +0530

    Adding Home Page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,32 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import UserWidget from "components/UserWidget";
+import { useSelector } from "react-redux";
 import NavBar from "scenes/navBar";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <NavBar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
